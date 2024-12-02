d22e8e5496549a059c65dde567a18632964a07a8
tfonstad <tfonstad@wisc.edu>
2024-12-01 20:20:30 -0600

    Add Room database setup with User entity and DAO, created login page that saves username
    - Implement User entity
    - Set up database access in LoginActivity for user authentication

--- app/src/main/java/com/cs407/badgerstudy/LoginActivity.kt
+++ app/src/main/java/com/cs407/badgerstudy/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.cs407.badgerstudy
 
 import android.content.Intent
+import android.widget.Toast
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
